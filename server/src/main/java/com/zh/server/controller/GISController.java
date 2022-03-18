ec9d53dbda27cce481d0674039a9932e87016bde
zhanghu-hu <1350777414@qq.com>
2022-03-18 19:43:33 +0800

    【功能修改】添加quartz任务调度框架

--- server/src/main/java/com/zh/server/controller/GISController.java
+++ server/src/main/java/com/zh/server/controller/GISController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * GIS相关处理
